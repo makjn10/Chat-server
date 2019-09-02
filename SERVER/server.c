85206e379c72e846817ad02dd240d122ef33a39b
MAYANK JAIN <makjn10@gmail.com>
2019-09-03 01:54:17 +0530

    Update server.c

--- SERVER/server.c
+++ SERVER/server.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <netdb.h>
-
-#include <string.h>
-#include <time.h>
+#include "server.h"
 
 #define PORT 8001
 
