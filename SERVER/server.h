5c419ed91e6ec62f625e25afdb34bf05aa48e85a
MAYANK JAIN <makjn10@gmail.com>
2019-09-03 00:10:37 +0530

    Update server.h

--- SERVER/server.h
+++ SERVER/server.h
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <string.h>
+#include <time.h>
+
 #include "server-getfile.h"
 #include "server-sendfile.h"
 #include "server-message.h"
